d80fbd2e1f14c24b700710acb9b90344572d3161
cookie_mumbles <cookie_mumbles@proton.me>
2022-12-07 22:14:06 +0100

    fix: repeated snackbar requests funkyness
    fixes: #9

--- js/ui/Snacbar.js
+++ js/ui/Snacbar.js
@@ -20,20 +20,47 @@ export function showSnacbar(text, type) {
   }
 }
 
+var latestSnaccbarTimeout = null
+var latestReshowTimeout = null
+
 function showSnac(text, svgElement) {
+  stopReshowTimer()
+
+  if (latestSnaccbarTimeout != null) {
+    closeAndReopenSnacbarWithNewInfo(text, svgElement)
+  } else {
+    var snackbar = document.getElementById("snackbar");
+
+    snackbar.classList.toggle("show");
+    snackbar.innerHTML = ""
+    snackbar.appendChild(svgElement)
+
+    snackbar.appendChild(
+      createElement('div', { class:'tweet_header'}, text),
+    )
+
+    // After 4 seconds, remove the show class from DIV
+    latestSnaccbarTimeout = setTimeout(hideSnac, 4000);
+  }
+}
+
+function stopReshowTimer() {
+  if (latestReshowTimeout != null) {
+    // if we got here either after forcing an existing one closed and opening a
+    // new one after it has closed. Or when trying to open a new one while we're
+    // still waiting for the hiding to be done, clear the reshow timer either way
+    clearTimeout(latestReshowTimeout)
+  }
+}
+
+function closeAndReopenSnacbarWithNewInfo(text, svgElement) {
+    clearTimeout(latestSnaccbarTimeout)
+    hideSnac()
+    latestReshowTimeout = setTimeout(showSnac, 500, text, svgElement)
+}
+
+function hideSnac() {
   var snackbar = document.getElementById("snackbar");
-
-  snackbar.classList.toggle("show");
-  snackbar.innerHTML = ""
-  snackbar.appendChild(svgElement)
-
-  snackbar.appendChild(
-    createElement('div', { class:'tweet_header'}, text),
-  )
-
-  // After 4 seconds, remove the show class from DIV
-  setTimeout(
-    function(){ snackbar.classList.toggle("show"); },
-    4000
-  );
+  snackbar.classList.toggle("show"); 
+  latestSnaccbarTimeout = null
 }
