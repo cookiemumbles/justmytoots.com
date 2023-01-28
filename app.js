18de365bb79224f0ea4daf0b6b36dd68b635fe9e
cookie_mumbles <cookie_mumbles@proton.me>
2023-01-28 21:03:10 +0100

    build: add app.js that simply builds for remotely setup

---
+++ app.js
@@ -0,0 +1,14 @@
+const { exec } = require("child_process");
+
+exec("npm run build", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
+
