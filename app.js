3723412398ac845cb7d75b2b95e6f6e0bcfe4ba6
cookie_mumbles <cookie_mumbles@proton.me>
2023-02-27 21:13:47 +0100

    build: migrate to node application for serving and building
    Since my hosting package does not allow me to run npm commands as part
    of a post-checkout step I was forced to manually run the build command
    for each new commit.
    
    This commit migrates to using an express application for serving the
    site, and also integrates running the 'build' command for every update.
    
    fixes: #69

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-const { exec } = require("child_process");
+const express = require('express')
+const app = express()
+const path = require('path')
+const fs = require('fs')
+const port = process.env.PORT || 8008
 
-exec("npm run build", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
+const {exec, execSync} = require('child_process')
+
+const logFile = "build.log"
+const outDir = path.join(__dirname, 'dist')
+if (!fs.existsSync(outDir)) {
+  console.log(`Building (log: ${logFile})...`)
+  execSync(`date > ${logFile}; npm run build >> ${logFile}`)
+} else {
+  console.log(`Building in background (log: ${logFile}).`)
+  console.log("Next person to call the site will get the updated version")
+  exec(`date > ${logFile}; npm run build >> ${logFile}`)
+}
+
+app.use(express.static(outDir))
+
+app.get('/:acct', (req, res) => {
+  console.log("requesting for:", req.params)
+  res.sendFile('index.html', {root: outDir });
 });
 
+app.listen(port)
+console.log(`== App is running on port: ${port} ==`)
